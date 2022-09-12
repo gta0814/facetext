5c6b31923924ddfe8e069a932b54910a58283e53
GTA0814 <gdt0814@gmail.com>
2022-09-11 20:53:19 -0600

    update

--- sketch.js
+++ sketch.js
@@ -9,8 +9,7 @@
 // ASCII image source text: https://editor.p5js.org/codingtrain/sketches/LNBpdYQHP
 // ASCII image weather API: https://editor.p5js.org/codingtrain/sketches/DhdqcoWn4
 
-//const density = "Ñ@#W$9876543210?!abc;:+=-,._                    ";
-const density = "                    _.,-=+:;cba!?0123456789$W#@Ñ";
+const density = "Ñ@#W$9876543210?!abc;:+=-,._                    ";
 // const density = '       .:-i|=+%O#@'
 // const density = '        .:░▒▓█';
 
@@ -19,20 +18,20 @@ let asciiDiv;
 
 function setup() {
   noCanvas();
-  gloria = createCapture(image);
-  gloria.size(64, 48);
+  video = createCapture(VIDEO);
+  video.size(64, 48);
   asciiDiv = createDiv();
 }
 
 function draw() {
-  gloria.loadPixels();
+  video.loadPixels();
   let asciiImage = "";
-  for (let j = 0; j < gloria.height; j++) {
-    for (let i = 0; i < gloria.width; i++) {
-      const pixelIndex = (i + j * gloria.width) * 4;
-      const r = gloria.pixels[pixelIndex + 0];
-      const g = gloria.pixels[pixelIndex + 1];
-      const b = gloria.pixels[pixelIndex + 2];
+  for (let j = 0; j < video.height; j++) {
+    for (let i = 0; i < video.width; i++) {
+      const pixelIndex = (i + j * video.width) * 4;
+      const r = video.pixels[pixelIndex + 0];
+      const g = video.pixels[pixelIndex + 1];
+      const b = video.pixels[pixelIndex + 2];
       const avg = (r + g + b) / 3;
       const len = density.length;
       const charIndex = floor(map(avg, 0, 255, 0, len));
